ef62f5e6adf698a6b40b3d159949bb774acab8d7
hungrybeagle <cyang@deltalearns.ca>
2021-12-15 13:18:02 -0800

    Create x02_display.py

---
+++ x02_display.py
@@ -0,0 +1,33 @@
+#!python3
+
+def displayString(board):
+  """
+  function should create a string that can be displayed using a print command
+  this function should have no actual print commands in it
+  
+  input:
+  list board: the game board data:
+  7 8 9
+  4 5 6
+  1 2 3
+  
+  eg 
+  board = [ 'O' , 0 , 0 , 'X' , 'O' , 0 , 0 , 0 , 'X' ] 
+  should display:
+  - - X
+  X O -
+  O - -
+  
+  return value
+  str the gameboard
+  """
+  return None
+
+def main():
+  board = [ 'O' , 0 , 0 , 'X' , 'O' , 0 , 0 , 0 , 'X' ] 
+  assert displayString(board) == "- - X\nX O -\nO - -"
+  board = [ 0 , 'O' , 'X' , 'O' , 'O' , 0 , 'X' , 0 , 'X' ] 
+  assert displayString(board) == "X - X\nO O -\n- O X"
+
+if __name__ == "__main__":
+  main()
