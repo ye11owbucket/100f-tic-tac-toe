2fc233728e0877fccdcfdcb931851a3b7fe4f1fc
hungrybeagle <cyang@deltalearns.ca>
2021-12-15 13:05:39 -0800

    Create x01_data.py

---
+++ x01_data.py
@@ -0,0 +1,57 @@
+#!python3
+
+""" 
+Create a reliable method for storing and retrieving data for the game.
+"""
+
+def read(square,board):
+  """
+  This will do a lookup to find out what is in a particular square
+  inputs: 
+  list board : list of the squares in the board
+    7 | 8 | 9
+    4 | 5 | 6
+    1 | 2 | 3
+  int square: the square you are checking for an X or an O
+  
+  return:
+  the str value corresponding to the content of the square being checked
+  None if the square is empty
+  """
+  return None
+
+def write(square,board,player):
+  """
+  inputs:
+  int square : the square you are checking for an X or an O
+  list board : the complete game board stored as a list
+  str player : either an 'X' or an 'O' 
+  
+  return:
+  the updated gameboard with the new data
+  
+  The function should update the contents of the gameboard if the square is
+  empty.  If the square is not empty, it should not change the gameboard and
+  should return the original, unchanged gameboard data
+  """
+  
+  return None
+
+def mainRead():
+  board = [ 0, 'X', 0, 'X', 'O', 'O', 0 , 0, 0]
+  assert read(board,3) == None
+  assert read(board,2) == 'X'
+  assert read(board,5) == 'O'
+
+def mainWrite():
+  board = [ 0,0,0,0,0,0,0,0,0,0]
+  assert write(1,board,'X') == [ 'X',0,0,0,0,0,0,0,0,0]
+  assert write(5,board,'O') == [ 'X',0,0,0,'O',0,0,0,0,0]
+  #next command should not do anything because square #5 is already occupied by an 'O'
+  assert write(5,board,'X') == [ 'X',0,0,0,'O',0,0,0,0,0]
+ 
+  
+  
+if __name__ == "__main__" :
+  mainRead()
+  mainWrite()
