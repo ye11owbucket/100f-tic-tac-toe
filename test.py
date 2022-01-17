c7b4c47b2b8ee09ff891c27f82431718b5774556
#1 wabungus fan <zacharym234@deltalearns.ca>
2022-01-17 14:19:10 -0800

    yuh

--- test.py
+++ test.py
@@ -1,17 +1,26 @@
-def read(square,board):
-  if 'O' in board[square]:
-    return 'O'
-  if 'X' in board[square]:
-    return 'X'
-  else:
-    return None
+def write(square,board,player):
+  square -= square
+  while True:
+    if 0 == board[square]:
+      board.pop(square)
+      board.insert(square,player)
+      return board
+    elif 'O' == board[square]:
+      return board
+    elif 'X' == board[square]:
+      return board
+    elif 0 not in board:
+      return False
+  
+def mainWrite():
+  board = [ 0,0,0,0,0,0,0,0,0,0]
+  assert write(0,board,'X') == [ 'X',0,0,0,0,0,0,0,0,0]
+  assert write(4,board,'O') == [ 'X',0,0,0,'O',0,0,0,0,0]
+  #next command should not do anything because square #5 is already occupied by an 'O'
+  assert write(4,board,'X') == [ 'X',0,0,0,'O',0,0,0,0,0]
+ 
 
-def mainRead():
-  board = [ 0, 'X', 0, 'X', 'O', 'O', 0 , 0, 0]
-  assert read(2,board) == None
-  assert read(1,board) == 'X'
-  assert read(4,board) == 'O'
-
-
-if __name__ == "__main__" :
-  mainRead()
+board = [ 0,0,0,0,0,0,0,0,0,0]
+print(write(0,board,'X'))
+print(write(4,board,'O'))
+print(write(4,board,'X'))
