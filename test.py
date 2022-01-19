92256bbf6949955b4e9b8d571b9379e7aab770eb
#1 wabungus fan <zacharym234@deltalearns.ca>
2022-01-19 14:52:51 -0800

    yuh

--- test.py
+++ test.py
@@ -1,18 +1,54 @@
-
-=======
 def displayString(board):
   kill = 0
-  print(board)
   if kill in board:
-    print(board)
     for i in board:
-      print(board)
       x = board.index(kill)
       board.pop(x)
       board.insert(x,'-')
       if kill not in board:
         break
-  fboard = "\n".join(board)
+  board.insert(1, " ")
+  board.insert(3, " ")
+  board.insert(5, "\n")
+  board.insert(7, " ")
+  board.insert(9, " ")
+  board.insert(11, "\n")
+  board.insert(13, " ")
+  board.insert(15, " ")
+  fboard = ''.join(board)
   return fboard
-board = [ 0 , 'O' , 'X' , 'O' , 'O' , 0 , 'X' , 0 , 'X' ] 
-print(displayString(board))
+
+def whoWins(board):
+  count = 0
+  if 'X' == board[0]:
+    if 'X' == board[1]:
+      if 'X' == board[2]:
+        return 'X'
+  if 'X' == board[0]:
+    if 'X' == board[4]:
+      if 'X' == board[8]:
+        return 'X'
+  if 'X' == board[0]:
+    if 'X' == board[3]:
+      if 'X' == board[6]:
+        return 'X'
+  if 'X' == board[3]:
+    if 'X' == board[4]:
+      if 'X' == board[5]:
+        return 'X'
+  if 'X' == board[6]:
+    if 'X' == board[7]:
+      if 'X' == board[8]:
+        return 'X'
+  if 'X' == board[1]:
+    if 'X' == board[4]:
+      if 'X' == board[7]:
+        return 'X'
+  if 'X' == board[2]:
+    if 'X' == board[4]:
+      if 'X' == board[6]:
+        return 'X'
+  else:
+    return "tragic"
+board = [ 'X',0,0,0,'X',0,'O','O','X']
+print(whoWins(board))
--- x03_winner.py
+++ x03_winner.py
@@ -12,15 +12,59 @@ None if there is no winner
 """
 
 def whoWins(board):
-  count = 0
-  if 'X' == board[0]:
-    if 'X' == board[1]:
-      if 'X' == board[2]:
+    kill = 0
+    if kill in board:
+      for i in board:
+        x = board.index(kill)
+        board.pop(x)
+        board.insert(x,'-')
+        if kill not in board:
+          break
+    a = board[0]
+    b = board[1]
+    c = board[2]
+    d = board[3]
+    e = board[4]
+    f = board[5]
+    g = board[6]
+    h = board[7]
+    i = board[8]
+    ### O's
+    if (a+b+c) == 'OOO':
+        return 'O'
+    if (a+e+i) == 'OOO':
+        return 'O'
+    if (a+d+g) == 'OOO':
+        return 'O'
+    if (d+e+f) == 'OOO':
+        return 'O'
+    if (g+h+i) == 'OOO':
+        return 'O'
+    if (g+e+c) == 'OOO':
+        return 'O'
+    if (b+e+h) == 'OOO':
+        return 'O'
+    if (c+f+i) == 'OOO':
+        return 'O'
+    ### X's
+    if (a+b+c) == 'XXX':
         return 'X'
-  count + 1
-  if count == 9:
-    return None
-
+    if (a+e+i) == 'XXX':
+        return 'X'
+    if (a+d+g) == 'XXX':
+        return 'X'
+    if (d+e+f) == 'XXX':
+        return 'X'
+    if (g+h+i) == 'XXX':
+        return 'X'
+    if (g+e+c) == 'XXX':
+        return 'X'
+    if (b+e+h) == 'XXX':
+        return 'X'
+    if (c+f+i) == 'XXX':
+        return 'X'
+    else:
+        return None
 
 def main():
   board = [ 'O','X',0,'X','O',0,'X',0,'O']
